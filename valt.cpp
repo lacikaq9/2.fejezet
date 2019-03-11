4f28b8af4a11bf9b9b9154a7f9286d12f338d99d
lacikaq9 <43678530+lacikaq9@users.noreply.github.com>
2019-03-11 22:11:08 +0100

    Update valt.cpp

--- valt.cpp
+++ valt.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+	int egesz = 1;
+  mut1 = &egesz;
+  mut2 = &mut1;
+  
+  int& mut3 = egesz;
+  
+  int tomb [3] = { 16, 2, 77};
+  int& tombr = tomb[3];
+   
+	
+}
