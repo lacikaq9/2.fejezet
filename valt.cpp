e6777b9e0979eff9e72318e988746bfc06c97590
lacikaq9 <43678530+lacikaq9@users.noreply.github.com>
2019-03-12 13:44:51 +0100

    Update valt.cpp

--- valt.cpp
+++ valt.cpp
@@ -1,16 +1,38 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
-	int egesz = 1;
-  mut1 = &egesz;
-  mut2 = &mut1;
-  
-  int& mut3 = egesz;
-  
-  int tomb [3] = { 16, 2, 77};
-  int& tombr = tomb[3];
-   
-	
+
+	// Int 
+	int a;
+
+	// Pointer to int
+	int *b = &a;
+
+	// Int referencia
+	int &r = a;
+
+	// 5 elemes tomb 
+	int c[5];
+
+	// Tomb referencia 
+	int (&tr)[5] = c;
+
+	// Egészre mutató mutatok tombje
+	int *d[5];
+
+	//mutatot mutato visszaado fuggveny
+	int *h ();
+
+	//mutatót mutató visszaadó függvényre mutató függvény
+	int *(*1) ();
+
+	//egészet visszaadó és két egészet kapó függvényre mutató mutatót visszaado, egeszet kapo fuggveny
+	int (*v ( int c)) (int a, int b)
+
+	//fuggveny mutato egy egeszet visszaado es ket egeszet kapo fuggvenyre 
+	//mutato mutatot visszaado, egeszet kapo fuggvényre
+	int (*(*z) (int)) (int, int);
+
+
 }
